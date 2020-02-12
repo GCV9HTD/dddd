19185abfcd18a0801d5d2a7723e61ca2651c0a3a
shyammvm <shyammvmpro@gmail.com>
2020-02-12 10:43:32 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,93 @@
+from scipy.spatial import distance as dist
+from imutils.video import VideoStream
+from imutils import face_utils
+from threading import Thread
+import numpy as np
+import playsound
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+
+
+def sound_alarm(path):
+    playsound.playsound(path)
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor",default="")
+ap.add_argument("-a", "--alarm", type=str, default="")
+ap.add_argument("-w", "--webcam", type=int, default=0)
+args = vars(ap.parse_args())
+
+def eye_aspect_ratio(eye):
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+    C = dist.euclidean(eye[0], eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
+
+
+ear_thresh = 0.25
+frame_thresh = 54
+
+counter = 0
+alarm_on = False
+
+detector = dlib.get_frontal_face_detector()
+args["shape_predictor"] ="shape_predictor_68_face_landmarks.dat"
+args["alarm"] ="alarm.wav"
+predictor = dlib.shape_predictor(args["shape_predictor"])
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+vs = VideoStream(src=args["webcam"]).start()
+time.sleep(1.0)
+
+while True:
+    frame = vs.read()
+    frame = imutils.resize(frame, width = 450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = detector(gray, 0)
+    for face in faces:
+        shape = predictor(gray, face)
+        shape = face_utils.shape_to_np(shape)
+        
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+
+        ear = (leftEAR + rightEAR) / 2.0
+        print(ear)
+        print(counter)
+        
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+        break
+
+    if ear < ear_thresh:
+        counter += 1
+        if counter > frame_thresh:
+            if args["alarm"] != "":
+                t = Thread(target=sound_alarm, args=(args["alarm"],))
+                t.deamon = True
+                t.start()		
+					
+        cv2.putText(frame, "DROWSINESS ALERT!", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        
+    
+    else:
+        counter = 0
+
+
+    cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    
+
+    
